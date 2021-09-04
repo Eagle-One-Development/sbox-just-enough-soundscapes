cac883b6b0c40fdf54f91b52688510c0fc8113f8
Adesi <kopietz123@gmail.com>
2021-09-04 07:20:27 +0200

    Pushed new OBB Fader + new testing Soundscape with free sounds

--- Code/Entity/Hammer/SoundScapeTagFader.cs
+++ Code/Entity/Hammer/SoundScapeTagFader.cs
@@ -1,31 +1,19 @@
 using System;
+using System.Diagnostics;
 using System.Diagnostics.Tracing;
+using Gamelib.Extensions;
 using Hammer;
 
 namespace Sandbox
 {
 
-	public abstract class BaseSoundScapeFader : Entity
+	public abstract partial class BaseSoundScapeFader : Entity
 	{
 		[ClientVar( "jess_debug_fader" )]
 		public static bool DebugFader { get; set; } = false;
 
-		public override void Spawn()
-		{
-			base.Spawn();
-			Transmit = TransmitType.Always;
-		}
-
-	}
-	[Library( "snd_soundscape_fader_point_jess" ), Sphere( "InnerRadius", 77, 147, 191 ), Sphere( "OuterRadius", 255, 255, 255 )]
-	public partial class SoundScapeTagFaderPoint : BaseSoundScapeFader
-	{
-
-		[Property, Net] public float InnerRadius { get; set; } = 50f;
-		[Property, Net] public float OuterRadius { get; set; } = 100f;
-
-		[Property, Net] public float MinValue { get; set; } = 0f;
-		[Property, Net] public float MaxValue { get; set; } = 1f;
+		[Property, Net] public float MinValue { get; set; } = 0.0f;
+		[Property, Net] public float MaxValue { get; set; } = 1.0f;
 
 
 
@@ -38,69 +26,26 @@ namespace Sandbox
 
 		public SoundScapeEntity soundScapeEntity;
 
-		public float InnerRadiusSqrt;
-		public float OuterRadiusSqrt;
+		public bool IsEnabled = true;
+		public bool IsInside = false;
 
-		public float CurrentValue = 0f;
+		public override void Spawn()
+		{
+			base.Spawn();
+			Transmit = TransmitType.Always;
+		}
 
 		public override void ClientSpawn()
 		{
-			InnerRadiusSqrt = InnerRadius * InnerRadius;
-			OuterRadiusSqrt = OuterRadius * OuterRadius;
-
+			base.ClientSpawn();
 			GetSoundscapeEntity();
 		}
+
 		private async void GetSoundscapeEntity()
 		{
 			await GameTask.NextPhysicsFrame();
 
 			soundScapeEntity = FindByName( SoundscapeEntity ) as SoundScapeEntity;
-		}
-
-		public bool IsInside = false;
-		public bool IsEnabled = true;
-
-		[Event.Tick.Client]
-		public void ClientTick()
-		{
-			if ( !IsEnabled ) return;
-			if ( DebugFader )
-			{
-				DebugOverlay.Sphere( Position, InnerRadius, Color.FromBytes( 77, 147, 191 ) );
-				DebugOverlay.Sphere( Position, OuterRadius, Color.FromBytes( 255, 255, 255 ) );
-				DebugOverlay.ScreenText( 0, $"{CurrentValue}" );
-			}
-			var Eyepos = Local.Pawn.EyePos;
-			if ( (Eyepos - Position).LengthSquared < OuterRadiusSqrt )
-			{
-				CurrentValue = MathX.LerpTo( MinValue, MaxValue, (Eyepos - Position).LengthSquared.Remap( InnerRadiusSqrt, OuterRadiusSqrt, 0f, 1f ) );
-				if ( CurrentValue.AlmostEqual( MinValue, 0.1f ) ) CurrentValue = MinValue;
-				IsInside = true;
-
-				if ( soundScapeEntity.IsValid() && SoundScape.SoundScapeEntity == soundScapeEntity )
-				{
-					SoundScape.FadeTagTo( FromTag, CurrentValue, 0f );
-				}
-				else if ( !soundScapeEntity.IsValid() )
-				{
-					SoundScape.FadeTagTo( FromTag, CurrentValue, 0f );
-				}
-			}
-			else if ( IsInside )
-			{
-				CurrentValue = MaxValue;
-				IsInside = false;
-
-				if ( soundScapeEntity.IsValid() && SoundScape.SoundScapeEntity == soundScapeEntity )
-				{
-					SoundScape.FadeTagTo( FromTag, MaxValue, 0f );
-				}
-				else if ( !soundScapeEntity.IsValid() )
-				{
-					SoundScape.FadeTagTo( FromTag, MaxValue, 0f );
-				}
-			}
-
 		}
 
 		[Input]
@@ -148,28 +93,182 @@ namespace Sandbox
 			}
 		}
 
+	}
+	[Library( "snd_soundscape_fader_point_jess" ), Sphere( "InnerRadius", 77, 147, 191 ), Sphere( "OuterRadius", 255, 255, 255 )]
+	public partial class SoundScapeTagFaderPoint : BaseSoundScapeFader
+	{
+
+		[Property, Net] public float InnerRadius { get; set; } = 50f;
+		[Property, Net] public float OuterRadius { get; set; } = 100f;
+
+
+
+
+
+		public float InnerRadiusSqrt;
+		public float OuterRadiusSqrt;
+
+		public float CurrentValue = 0f;
+
+		public override void ClientSpawn()
+		{
+			InnerRadiusSqrt = InnerRadius * InnerRadius;
+			OuterRadiusSqrt = OuterRadius * OuterRadius;
+		}
+
+
+
+
+
+		[Event.Tick.Client]
+		public void ClientTick()
+		{
+			if ( !IsEnabled ) return;
+			if ( DebugFader )
+			{
+				DebugOverlay.Sphere( Position, InnerRadius, Color.FromBytes( 77, 147, 191 ) );
+				DebugOverlay.Sphere( Position, OuterRadius, Color.FromBytes( 255, 255, 255 ) );
+				DebugOverlay.ScreenText( 0, $"{CurrentValue}" );
+			}
+			var Eyepos = Local.Pawn.EyePos;
+			if ( (Eyepos - Position).LengthSquared < OuterRadiusSqrt )
+			{
+				CurrentValue = MathX.LerpTo( MinValue, MaxValue, (Eyepos - Position).LengthSquared.Remap( InnerRadiusSqrt, OuterRadiusSqrt, 0f, 1f ) );
+				if ( CurrentValue.AlmostEqual( MinValue, 0.1f ) ) CurrentValue = MinValue;
+				IsInside = true;
+
+				if ( soundScapeEntity.IsValid() && SoundScape.SoundScapeEntity == soundScapeEntity )
+				{
+					SoundScape.FadeTagTo( FromTag, CurrentValue, 0f );
+				}
+				else if ( !soundScapeEntity.IsValid() )
+				{
+					SoundScape.FadeTagTo( FromTag, CurrentValue, 0f );
+				}
+			}
+			else if ( IsInside )
+			{
+				CurrentValue = MaxValue;
+				IsInside = false;
+
+				if ( soundScapeEntity.IsValid() && SoundScape.SoundScapeEntity == soundScapeEntity )
+				{
+					SoundScape.FadeTagTo( FromTag, MaxValue, 0f );
+				}
+				else if ( !soundScapeEntity.IsValid() )
+				{
+					SoundScape.FadeTagTo( FromTag, MaxValue, 0f );
+				}
+			}
+
+		}
+
+
+
 
 
 
 	}
-	[Library( "snd_soundscape_fader_aabb_jess" ), BoxOrient( "inner_mins", "inner_maxs", 50 ), BoxOrient( "outer_mins", "outer_maxs", 100 )]
-	public partial class SoundScapeTagFaderAABB : BaseSoundScapeFader
+	[Library( "snd_soundscape_fader_obb_jess" ), BoxOrient( "inner_mins", "inner_maxs", 50 ), BoxOrient( "outer_mins", "outer_maxs", 100 )]
+	public partial class SoundScapeTagFaderOBB : BaseSoundScapeFader
 	{
 		[Property( Hammer = false ), Net] public Vector3 inner_mins { get; set; } = new( -1000 );
 		[Property( Hammer = false ), Net] public Vector3 inner_maxs { get; set; } = new( 1000 );
 		[Property( Hammer = false ), Net] public Vector3 outer_mins { get; set; } = new( -500 );
 		[Property( Hammer = false ), Net] public Vector3 outer_maxs { get; set; } = new( 500 );
 
+
+		BBox innerbbox;
+		BBox outerbbox;
+
+		public float CurrentValue = 0f;
+
+		public override void ClientSpawn()
+		{
+			base.ClientSpawn();
+			innerbbox = new( inner_mins, inner_maxs );
+			outerbbox = new( outer_mins, outer_maxs );
+		}
+
 		[Event.Tick.Client]
 		public void ClientTick()
 		{
+			if ( !IsEnabled ) return;
 
+			if ( DebugFader )
+			{
+				var outerbboxWorld = outerbbox.ToWorldSpace( this );
+				DebugOverlay.Box( outerbboxWorld.Mins, outerbboxWorld.Maxs, Color.White, 0f );
+				var innerbboxWorld = innerbbox.ToWorldSpace( this );
+				DebugOverlay.Box( innerbboxWorld.Mins, innerbboxWorld.Maxs, Color.Blue, 0f );
+
+
+				DebugOverlay.ScreenText( CurrentValue.ToString() );
+			}
+
+			var Eyepos = Local.Pawn.EyePos;
+			if ( outerbbox.Contains( new( Transform.PointToLocal( Eyepos ) ) ) )
+			{
+				IsInside = true;
+
+				var innerPoint = innerbbox.ClosestPointToWorldSpace( Transform, Local.Pawn.EyePos );
+				var outerPoint = outerbbox.ClosestPointToWorldSpace( Transform, Local.Pawn.EyePos );
+				if ( DebugFader )
+				{
+					DebugOverlay.Line( innerPoint, outerPoint );
+					//DebugOverlay.ScreenText( 0, (innerPoint - outerPoint).Length.ToString() );
+					//DebugOverlay.ScreenText( 1, (innerPoint - Local.Pawn.EyePos).Length.ToString() );
+
+				}
+				if ( innerbbox.Contains( new( Transform.PointToLocal( Eyepos ) ) ) )
+				{
+					CurrentValue = MaxValue;
+					return;
+				}
+
+
+				CurrentValue = MathX.LerpTo( MaxValue, MinValue, (innerPoint - Local.Pawn.EyePos).Length.Remap( 0, (innerPoint - outerPoint).Length, 0, 1 ) );
+				if ( DebugFader ) DebugOverlay.Line( innerPoint, Local.Pawn.EyePos );
+
+
+				if ( soundScapeEntity.IsValid() && SoundScape.SoundScapeEntity == soundScapeEntity )
+				{
+					SoundScape.FadeTagTo( FromTag, CurrentValue, 0f );
+				}
+				else if ( !soundScapeEntity.IsValid() )
+				{
+					SoundScape.FadeTagTo( FromTag, CurrentValue, 0f );
+				}
+
+
+
+			}
+			else if ( IsInside )
+			{
+				IsInside = false;
+
+				CurrentValue = MinValue;
+				if ( innerbbox.Contains( new( Transform.PointToLocal( Eyepos ) ) ) )
+				{
+					CurrentValue = MaxValue;
+				}
+
+				if ( soundScapeEntity.IsValid() && SoundScape.SoundScapeEntity == soundScapeEntity )
+				{
+					SoundScape.FadeTagTo( FromTag, CurrentValue, 0f );
+				}
+				else if ( !soundScapeEntity.IsValid() )
+				{
+					SoundScape.FadeTagTo( FromTag, CurrentValue, 0f );
+				}
+			}
 		}
 
 
+
 	}
-	[Library( "snd_soundscape_fader_direction_aabb_jess" ), BoxOrient( "inner_mins", "inner_maxs", 50 ), BoxOrient( "outer_mins", "outer_maxs", 100 ), DrawAngles( "DirectionAngle", "angles" )]
-	public partial class SoundScapeTagFaderDirectionalAABB : BaseSoundScapeFader
+	[Library( "snd_soundscape_fader_direction_obb_jess" ), BoxOrient( "inner_mins", "inner_maxs", 50 ), BoxOrient( "outer_mins", "outer_maxs", 100 ), DrawAngles( "DirectionAngle", "angles" )]
+	public partial class SoundScapeTagFaderDirectionalOBB : BaseSoundScapeFader
 	{
 		[Property( Hammer = false ), Net] public Vector3 inner_mins { get; set; } = new( -1000 );
 		[Property( Hammer = false ), Net] public Vector3 inner_maxs { get; set; } = new( 1000 );
@@ -180,7 +279,7 @@ namespace Sandbox
 		[Event.Tick.Client]
 		public void ClientTick()
 		{
-
+			if ( !IsEnabled ) return;
 		}
 
 
---
+++ Code/Extensions/BBoxExtension.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Sandbox;
+
+namespace Gamelib.Extensions
+{
+	public static class BBoxExtension
+	{
+		public static bool ContainsXY( this BBox a, BBox b )
+		{
+			return (
+				b.Mins.x >= a.Mins.x && b.Maxs.x < a.Maxs.x &&
+				b.Mins.y >= a.Mins.y && b.Maxs.y < a.Maxs.y
+			); ;
+		}
+
+		public static bool Contains( this BBox a, BBox b )
+		{
+			return (
+				b.Mins.x >= a.Mins.x && b.Maxs.x < a.Maxs.x &&
+				b.Mins.y >= a.Mins.y && b.Maxs.y < a.Maxs.y &&
+				b.Mins.z >= a.Mins.z && b.Maxs.z < a.Maxs.z
+			); ;
+		}
+
+		public static bool Overlaps( this BBox bbox, Vector3 position, float radius )
+		{
+			var dmin = 0f;
+			var bmin = bbox.Mins;
+			var bmax = bbox.Maxs;
+
+			if ( position.x < bmin.x )
+			{
+				dmin += MathF.Pow( position.x - bmin.x, 2 );
+			}
+			else if ( position.x > bmax.x )
+			{
+				dmin += MathF.Pow( position.x - bmax.x, 2 );
+			}
+
+			if ( position.y < bmin.y )
+			{
+				dmin += MathF.Pow( position.y - bmin.y, 2 );
+			}
+			else if ( position.y > bmax.y )
+			{
+				dmin += MathF.Pow( position.y - bmax.y, 2 );
+			}
+
+			if ( position.z < bmin.z )
+			{
+				dmin += MathF.Pow( position.z - bmin.z, 2 );
+			}
+			else if ( position.z > bmax.z )
+			{
+				dmin += MathF.Pow( position.z - bmax.z, 2 );
+			}
+
+			return dmin <= MathF.Pow( radius, 2 );
+		}
+
+		public static bool Overlaps( this BBox a, BBox b )
+		{
+			return (
+				a.Mins.x < b.Maxs.x && b.Mins.x < a.Maxs.x &&
+				a.Mins.y < b.Maxs.y && b.Mins.y < a.Maxs.y &&
+				a.Mins.z < b.Maxs.z && b.Mins.z < a.Maxs.z
+			); ;
+		}
+
+		public static BBox ToWorldSpace( this BBox bbox, Entity entity )
+		{
+			return new BBox
+			{
+				Mins = entity.Transform.PointToWorld( bbox.Mins ),
+				Maxs = entity.Transform.PointToWorld( bbox.Maxs )
+			};
+		}
+
+		public static Vector3 ClosestPointToLocalSpace( this BBox box, Transform transform, Vector3 point )
+		{
+
+			var directionVector = transform.PointToLocal( point ) - box.Center;
+
+			var distanceX = 0f;
+			var distanceY = 0f;
+			var distanceZ = 0f;
+
+			if ( box.Contains( new( transform.PointToLocal( point ) ) ) )
+			{
+				distanceX = Vector3.Dot( directionVector, transform.Rotation.Forward );
+				distanceX += directionVector.Normal.x * box.Size.x * 0.5f;
+				if ( distanceX > box.Size.x * 0.5f ) distanceX = box.Size.x * 0.5f;
+				else if ( distanceX < -box.Size.x * 0.5f ) distanceX = -box.Size.x * 0.5f;
+
+				distanceY = Vector3.Dot( directionVector, transform.Rotation.Left );
+				distanceY += directionVector.Normal.y * box.Size.y * 0.5f;
+				if ( distanceY > box.Size.y * 0.5f ) distanceY = box.Size.y * 0.5f;
+				else if ( distanceY < -box.Size.y * 0.5f ) distanceY = -box.Size.y * 0.5f;
+
+				distanceZ = Vector3.Dot( directionVector, transform.Rotation.Up );
+				distanceZ += directionVector.Normal.z * box.Size.z * 0.5f;
+				if ( distanceZ > box.Size.z * 0.5f ) distanceZ = box.Size.z * 0.5f;
+				else if ( distanceZ < -box.Size.z * 0.5f ) distanceZ = -box.Size.z * 0.5f;
+
+			}
+			else
+			{
+				distanceX = Vector3.Dot( directionVector, transform.Rotation.Forward );
+				if ( distanceX > box.Size.x * 0.5f ) distanceX = box.Size.x * 0.5f;
+				else if ( distanceX < -box.Size.x * 0.5f ) distanceX = -box.Size.x * 0.5f;
+
+				distanceY = Vector3.Dot( directionVector, transform.Rotation.Left );
+				if ( distanceY > box.Size.y * 0.5f ) distanceY = box.Size.y * 0.5f;
+				else if ( distanceY < -box.Size.y * 0.5f ) distanceY = -box.Size.y * 0.5f;
+
+				distanceZ = Vector3.Dot( directionVector, transform.Rotation.Up );
+				if ( distanceZ > box.Size.z * 0.5f ) distanceZ = box.Size.z * 0.5f;
+				else if ( distanceZ < -box.Size.z * 0.5f ) distanceZ = -box.Size.z * 0.5f;
+			}
+
+			return box.Center + distanceX * transform.Rotation.Forward + distanceY * transform.Rotation.Left + distanceZ * transform.Rotation.Up;
+		}
+		public static Vector3 ClosestPointToWorldSpace( this BBox box, Transform transform, Vector3 point )
+		{
+			return transform.PointToWorld( box.ClosestPointToLocalSpace( transform, point ) );
+		}
+	}
+}
